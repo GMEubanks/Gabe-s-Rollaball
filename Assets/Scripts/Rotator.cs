9659dae5464fbba9b311b6bfa69d4dd07ad10696
camden-obertop <camden.obertop@gmail.com>
2021-02-01 16:04:48 -0600

    Changed the required count for winning, and randomized the color of the pickups.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    private MeshRenderer _pickupColor;
+
+    private void Start()
+    {
+        _pickupColor = GetComponent<MeshRenderer>();
+        StartCoroutine(RandomizeColor());
+    }
    
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);  
+    }
+
+    IEnumerator RandomizeColor()
+    {
+        while (true)
+        {
+            Debug.Log("aaa");
+            _pickupColor.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            yield return new WaitForSeconds(Random.Range(.5f, 3f));
+        }
     }
 }
